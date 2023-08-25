d0a5eb90b85d5941271eafa2a605bed2af3b5faf
HarshitkumarTesting <136461929+HarshitkumarTesting@users.noreply.github.com>
2023-08-25 15:15:20 +0530

    Update GIT_DEMO.java

--- Git_Demo2/src/GIT_DEMO.java
+++ Git_Demo2/src/GIT_DEMO.java
@@ -6,6 +6,11 @@ public static void main(String[] args)
 {
 	System.out.println("Harshit");
 	System.out.println("Kumar");
+
+	System.out.println("Yogesh");
+	System.out.println("Kiran");
+	System.out.println("Maddy");
+	System.out.println("Harry");
 }
 
 }
